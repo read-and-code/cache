9644f4d93cb5a06b888ac3bc77614d5b33bc4660
Frederick-S <mao_xiaodan@hotmail.com>
2019-02-10 17:35:42 +0800

    go fmt

--- cache/cache_status.go
+++ cache/cache_status.go
@@ -8,7 +8,7 @@ type CacheStatus struct {
 	ValueSize int64
 }
 
-func (cacheStatus *CacheStatus) add(key string, value []byte)  {
+func (cacheStatus *CacheStatus) add(key string, value []byte) {
 	cacheStatus.Count += 1
 	cacheStatus.KeySize += int64(len(key))
 	cacheStatus.ValueSize += int64(len(value))
